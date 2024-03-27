20aad4b282162e6eb6621097e83a64cd43317bb3
abrullc <adrianbrullcontreras@gmail.com>
2024-03-27 10:25:23 +0100

    Proyecto de prueba en Angular 17

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  public title: string = 'Hola Mundo';
+}
